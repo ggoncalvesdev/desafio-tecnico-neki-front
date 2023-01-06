93b781e52f59ff5768665ae52a6a12c4329a199c
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2023-01-05 22:45:18 -0300

    feat(Registration):create

---
+++ src/pages/registration/index.tsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+
+import * as Animatable from "react-native-animatable";
+import { Ionicons } from "@expo/vector-icons";
+import {
+    Image,
+    ImageBackground,
+    SafeAreaView,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    useColorScheme,
+    View,
+} from "react-native";
+
+import { styles } from "./styles";
+import { Alerta } from "../../components/alert";
+import Seta from "../../assets/icons/seta-direita.png";
+import authService, { createUser } from "../../services/Api/Request/authService";
+
+export function Registration({ navigation }) {
+    const [login, setLogin] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [validPass, setValidPass] = useState(null);
+    const [hidePass, setHidePass] = useState(true);
+
+    const colorScheme = useColorScheme();
+    const themeTextStyle = colorScheme === "light" ? styles.lightThemeText : styles.darkThemeText;
+    const themeContainerStyle = colorScheme === "light" ? styles.lightThemeContainer : styles.darkThemeContainer;
+
+    function salvar() {
+        let data: createUser = {
+            login: login,
+            password: password,
+        };
+        console.log(data);
+
+        if (password == validPass) {
+            authService
+                .create(data)
+                .then((res) => {
+                    console.log(res.data);
+                    Alerta("Parabéns!", "você foi cadastrado com sucesso!");
+                    navigation.navigate("Login");
+                })
+                .catch((err) => {
+                    Alerta("Oops!", " Este e-mail já está em uso, verique-o ou recupere sua senha");
+                    console.log(err);
+                });
+        } else {
+            Alerta("Oops!", "As senhas não coincidem");
+        }
+    }
+
+    return (
+        <View style={[styles.container, themeContainerStyle]}>
+            <SafeAreaView>
+                <Animatable.View animation={"fadeInLeft"} delay={500} style={styles.containerTitulo}>
+                    <Text style={[styles.subTitulo, themeTextStyle]}>Entre no App</Text>
+                    <Text style={[styles.titulo, themeTextStyle]}>NekiBook</Text>
+                </Animatable.View>
+
+                <Animatable.View animation="fadeInUp" style={styles.containerImput}>
+                    <TextInput
+                        placeholder="Login"
+                        style={styles.input}
+                        onChangeText={(value) => {
+                            setLogin(value);
+                        }}
+                        accessibilityHint="Insira seu Login."
+                    />
+                    <TextInput
+                        placeholder="Senha"
+                        secureTextEntry={hidePass}
+                        style={styles.input}
+                        onChangeText={(value) => {
+                            setPassword(value);
+                        }}
+                        accessibilityHint="Insira uma senha que tenha seis caracteres."
+                    ></TextInput>
+
+                    <TouchableOpacity style={styles.eyevisible} onPress={() => setHidePass(!hidePass)}>
+                        {hidePass ? (
+                            <>
+                                <Ionicons name="eye-off-outline" size={24} color="#0050AF" />
+                            </>
+                        ) : (
+                            <>
+                                <Ionicons name="eye-outline" size={24} color="#0050AF" />
+                            </>
+                        )}
+                    </TouchableOpacity>
+                    <TextInput
+                        placeholder="Validar senha"
+                        secureTextEntry={true}
+                        style={styles.input}
+                        onChangeText={(value) => {
+                            setValidPass(value);
+                        }}
+                        accessibilityHint="Insira sua senha novamente para valida-la."
+                    />
+                </Animatable.View>
+                <View style={styles.containerBotao}>
+                    <TouchableOpacity
+                        activeOpacity={0.7}
+                        onPress={() => {
+                            salvar();
+                        }}
+                        accessibilityLabel="Botão cadastro."
+                        accessibilityHint="Clique aqui para finalizar o seu cadastro."
+                    >
+                        <View style={styles.containerTextoBotao}>
+                            <Text style={[styles.textoBotao, themeTextStyle]}>Cadastrar</Text>
+                            <Image source={Seta} style={[styles.setaBotao, themeContainerStyle]} />
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+        </View>
+    );
+}
---
+++ src/pages/registration/styles.ts
@@ -0,0 +1,96 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    lightThemeContainer: {
+        backgroundColor: "#fff",
+        tintColor: "#0050AF",
+    },
+    darkThemeContainer: {
+        backgroundColor: "#0050AF",
+        tintColor: "#fff",
+    },
+    lightThemeText: {
+        color: "#0050AF",
+    },
+    darkThemeText: {
+        color: "#fff",
+    },
+    container: {
+        flex: 1,
+    },
+    containerImput: {
+        marginTop: 15,
+        paddingHorizontal: 40,
+    },
+    containerBotao: {
+        justifyContent: "center",
+        width: "100%",
+        top: 70,
+    },
+    containerTextoBotao: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        height: 100,
+    },
+    setaBotao: {
+        marginTop: 8,
+        width: 25,
+        height: 25,
+    },
+    textoBotao: {
+        marginRight: 8,
+        fontSize: 30,
+    },
+    containerLogo: {
+        marginTop: "3%",
+        paddingHorizontal: "10%",
+    },
+    containerTitulo: {
+        marginVertical: "12%",
+        alignItems: "center",
+    },
+    containerHeader: {
+        marginTop: "14%",
+        marginBotton: "8%",
+        paddingStart: "5%",
+    },
+    titulo: {
+        textAlign: "center",
+        fontSize: 39,
+        fontWeight: "bold",
+    },
+    subTitulo: {
+        textAlign: "center",
+        fontSize: 16,
+    },
+    input: {
+        backgroundColor: "#ffff",
+        paddingVertical: "4%",
+        marginBottom: "6%",
+        borderRadius: 15,
+        padding: 60,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 4.65,
+
+        elevation: 6,
+    },
+    eyevisible: {
+        position: "absolute",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        right: 55,
+        bottom: 110,
+        width: 50,
+    },
+    textoLink: {
+        color: "#211F1F",
+        fontSize: 15,
+        fontWeight: "bold",
+    },
+});
